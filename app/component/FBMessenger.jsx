8ea1437cb5408ecd9e75ae95920da3aa8cb46f90
Basel Diab <baseldiab21@gmail.com>
2024-06-27 16:27:59 +0300

    adding

--- app/component/FBMessenger.jsx
+++ app/component/FBMessenger.jsx
@@ -1,12 +1,14 @@
 'use client'
-import { CustomChat, FacebookProvider } from 'react-facebook';
+import React, { Component} from 'react';
+import { FacebookProvider, SendToMessenger  } from 'react-facebook';
 
 const FBMessenger = () => {
+
   return (
-    <div className='bg-white'>
-    <FacebookProvider appId="767148212239480" chatSupport>
-        <CustomChat pageId="384657458055394" minimized={true}/>
-      </FacebookProvider>
+    <div className='bg-white w-20 h-20'>
+    <FacebookProvider appId="123456789">
+        <SendToMessenger messengerAppId="123456789" pageId="123456789"/>
+      </FacebookProvider>  
     </div>
   )
 }
